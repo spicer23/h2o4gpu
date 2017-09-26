bc45c8871d751e52104d7f5d74e027f3fec7742b
navdeep-G <mr.navdeepgill@gmail.com>
2017-09-26 13:49:30 -0700

    Update Makefile and header

--- src/gpu/svd/svd.h
+++ src/gpu/svd/svd.h
@@ -8,22 +8,5 @@
 #ifndef SVD_H_
 #define SVD_H_
 
-template<typename T>
-T svd(cusolverDnHandle_t handle,
-		signed char jobu,
-		signed char jobvt,
-		int m, int n,
-		double *A,
-		int lda,
-		double *S,
-		double *U,
-		int ldu,
-		double *VT,
-		int ldvt,
-		double *work,
-		int lwork,
-		double *rwork,
-		int *info );
-
 #endif /* SVD_H_ */
 

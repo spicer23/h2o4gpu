3a78ca261a7365e7e025b665dd06411562eda95d
navdeep-G <mr.navdeepgill@gmail.com>
2017-09-27 16:39:16 -0700

    Add lib py file for svd

---
+++ src/interface_py/h2o4gpu/libs/lib_svd.py
@@ -0,0 +1,66 @@
+"""
+:copyright: 2017 H2O.ai, Inc.
+:license:   Apache License Version 2.0 (see LICENSE for details)
+"""
+from ctypes import c_int, c_size_t, cdll
+from h2o4gpu.types import c_float_p, c_void_pp, c_double_p
+
+
+class CPUlib:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get():
+        from h2o4gpu.libs.lib_utils import cpu_lib_path
+
+        return _load_svd_lib(cpu_lib_path())
+
+
+class GPUlib:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get():
+        from h2o4gpu.libs.lib_utils import gpu_lib_path
+
+        return _load_svd_lib(gpu_lib_path())
+
+'''
+int sourceDev, int datatype, int sharedA,
+			int nThreads, int gpu_id, int nGPUs, int totalnGPUs, const char ord, size_t mTrain, size_t n,
+			int standardize, int verbose, void *matrix
+'''
+def _load_svd_lib(lib_path):
+    """Load the underlying C/C++ SVD library using cdll.
+
+    :param lib_path: Path to the library file
+    :return: object representing the loaded library
+    """
+    try:
+        h2o4gpu_svd_lib = cdll.LoadLibrary(lib_path)
+
+        h2o4gpu_svd_lib.make_ptr_double.argtypes = [
+            c_int, c_int, c_int, c_int, c_int, c_int, c_int,
+            c_int, c_size_t, c_size_t, c_int, c_int, c_void_pp
+        ]
+        h2o4gpu_svd_lib.make_ptr_double.restype = c_int
+
+        h2o4gpu_svd_lib.make_ptr_float.argtypes = [
+            c_int, c_int, c_int, c_int, c_int, c_int, c_int,
+            c_int, c_size_t, c_size_t, c_int, c_int, c_void_pp
+        ]
+        h2o4gpu_svd_lib.make_ptr_float.restype = c_int
+
+# pylint: disable=broad-except
+    except Exception as e:
+        print("Exception")
+        print(e)
+        print('\nWarning: h2o4gpu_svd_lib shared object (dynamic library) %s '
+              'failed to load. ' % lib_path)
+        h2o4gpu_svd_lib = None
+
+    return h2o4gpu_svd_lib
